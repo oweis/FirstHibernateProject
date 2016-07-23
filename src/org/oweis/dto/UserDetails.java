700e312548d3a09c0e6ce83f07d367fa9df53b10
oweis <oweisyahya@gmail.com>
2016-07-23 02:19:00 +0200

    Version 4 : OneToMany

--- src/org/oweis/dto/UserDetails.java
+++ src/org/oweis/dto/UserDetails.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.CollectionId;
@@ -25,15 +26,9 @@ public class UserDetails {
 	private int userId;
 	@Column (name="USER_NAME")
 	private String userName;
-	@ElementCollection
-	@JoinTable(name="User_Address",
-		joinColumns = @JoinColumn(name="User_Id"))
-	@GenericGenerator(name = "hilo-gen", strategy = "hilo")
-	@CollectionId(columns = { @Column(name="Address_id") }, generator = "hilo-gen", type = @Type(type = "long"))
-	private Collection<Address> listOfAddresses = new ArrayList<Address>();
-	@OneToOne
-	@JoinColumn(name="VEHICULE_ID")
-	private Vehicule vehicule;
+	@OneToMany
+	@JoinTable(name="User_Vehicule",joinColumns=@JoinColumn(name="User_id"),inverseJoinColumns=@JoinColumn(name="Vehicule_id"))
+	private Collection<Vehicule> vehicule = new ArrayList<Vehicule>();
 	
 	public UserDetails(){}
 	
@@ -43,20 +38,12 @@ public class UserDetails {
 		this.userName = userName;
 	}
 
-	public Vehicule getVehicule() {
+	public Collection<Vehicule> getVehicule() {
 		return vehicule;
 	}
 
-	public void setVehicule(Vehicule vehicule) {
+	public void setVehicule(Collection<Vehicule> vehicule) {
 		this.vehicule = vehicule;
-	}
-
-	public Collection<Address> getListOfAddresses() {
-		return listOfAddresses;
-	}
-
-	public void setListOfAddresses(Collection<Address> listOfAddresses) {
-		this.listOfAddresses = listOfAddresses;
 	}
 
 	public int getUserId() {
--- src/org/oweis/dto/hibernate/HibernateTest.java
+++ src/org/oweis/dto/hibernate/HibernateTest.java
@@ -13,33 +13,25 @@ public class HibernateTest {
 		
 		UserDetails	user = new UserDetails();
 		
-		Vehicule vehicule = new Vehicule();
+		Vehicule vehicule1 = new Vehicule();
+		Vehicule vehicule2 = new Vehicule();
+		
+		vehicule1.setVehiculeName("Dacia");
+		vehicule2.setVehiculeName("Ford");
+		user.getVehicule().add(vehicule1);
+		user.getVehicule().add(vehicule2);
+		
 
-		vehicule.setVehiculeName("Car");
-		user.setVehicule(vehicule);
-		
-		Address addr = new Address();
-		addr.setStreet("street");
-		addr.setCity("city");
-		addr.setPincode("0123");
-		addr.setState("state");
-		
-		Address addr2 = new Address();
-		addr2.setStreet("street 2");
-		addr2.setCity("city 2");
-		addr2.setPincode("0123 2");
-		addr2.setState("state 2");
 		
 		user.setUserName("Oweis");
-		user.getListOfAddresses().add(addr);
-		user.getListOfAddresses().add(addr2);
-			
+
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 	
 		session.beginTransaction();
 		session.save(user);
-		session.save(vehicule);
+		session.save(vehicule1);
+		session.save(vehicule2);
 		session.getTransaction().commit();
 		session.close();
 	
